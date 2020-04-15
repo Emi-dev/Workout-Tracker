f59423b93fdeaefd9e3c7955057ffb19cc57cc1f
Emi Clar <kotaki@gmail.com>
2020-04-15 17:13:58 -0400

    modified server.js

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds011321.mlab.com:11321/heroku_jlwbgrjn", { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
